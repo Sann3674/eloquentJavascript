83c2ad8dabfd0ff9528dbd3dc4e1eb81ecaec205
Sann3674 <75644750+Sann3674@users.noreply.github.com>
2021-12-27 12:46:08 +0000

    Update mathMin.js

--- mathMin.js
+++ mathMin.js
@@ -1,1 +1,9 @@
 //Instructions: create a function that performs the same task as Math.min
+
+function returnMin(x, y) {
+    if (x < y) {
+        return x;
+    }
+    else {
+    return y;
+    }};
