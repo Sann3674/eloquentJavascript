9646109604a8311966b028fd1b02c180776151a7
Sann3674 <75644750+Sann3674@users.noreply.github.com>
2021-12-27 18:57:34 +0000

    Update sumRange.js

--- sumRange.js
+++ sumRange.js
@@ -16,3 +16,24 @@ function sumArray (array) {
     }
     return total;
 };
+
+//bonus function
+function listArray (start, end, step=1) {
+    let newArray = [];
+    let difference = end - start;
+    let absDiff = Math.abs(difference);
+    if (difference > 0) {
+        for (let i = 0; i <= absDiff; i += step) {
+            newArray.push((start + i));
+        }
+    }
+    else if (difference < 0) {
+        for (let i = 0; i <= absDiff; i += step) {
+            newArray.push((start - i));
+        }
+    }
+    else {
+        newArray.push(start);
+    }
+    return newArray;
+};
