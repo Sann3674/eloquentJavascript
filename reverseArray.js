7b7f67af2d7d8ed070048f02b22c9f7be7536912
Sann3674 <75644750+Sann3674@users.noreply.github.com>
2021-12-28 13:46:13 +0000

    Update reverseArray.js

--- reverseArray.js
+++ reverseArray.js
@@ -1,3 +1,7 @@
+//Arrays have a reverse method that changes the array by inverting the order in which its elements appear. 
+//For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. 
+//The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. Neither may use the standard reverse method.
+
 function reverse(array) {
     let length = array.length;
     let newArray = [];
@@ -7,3 +11,13 @@ function reverse(array) {
     }
     return newArray;
 };
+
+function reverseInPlace(array) {
+    let halfLength = Math.floor((array.length / 2));
+    for (let i = 0; i < halfLength; i++) {
+        let tmp = array[i];
+        array[i] = array[(array.length - 1 - i)];
+        array[(array.length - 1 - i)] = tmp;
+         }
+    return array;
+         };
